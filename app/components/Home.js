cdfd74e94053490d9fa8736c9b0a1bf0e4fff4cd
Benjamin San Souci <benjamin.sansouci@gmail.com>
2018-08-07 16:09:12 -0700

    Wow this is basically imessage !

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,6 +8,7 @@ export default class Home extends Component<Props> {
   props: Props;
 
   render() {
+    let bubbles = [{body: "I'm a message", yours: false}, {body: "I'm another message", yours: true}];
     return (
       <div className={styles.container} data-tid="container">
         <div className={styles.left_column}>
@@ -18,14 +19,20 @@ export default class Home extends Component<Props> {
         </div>
         <div className={styles.right_column}>
           <div className={styles.chat_window}>
-            <div className={styles.chat_bubble}>
-            </div>  
+            {bubbles.map(({body, yours}) => 
+              <div className={[styles.chat_bubble, yours ? styles.yours : styles.theirs].join(' ')}>
+                <span>
+                {body}
+                </span> 
+              </div>
+            )}
           </div>
           <div className={styles.chat_controls}>
-          <div className={styles.chat_input}>
-          </div>
-          <div className={styles.chat_send}>
-          </div>
+            <input className={styles.chat_input}>
+            </input>
+            <button className={styles.chat_send}>
+            send
+            </button>
           </div>
         </div>
       </div>
